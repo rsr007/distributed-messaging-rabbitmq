53111f7d3dcc9bd5553c46746450015142be83b3
rsr007 <rsr7294@gmail.com>
2025-07-13 11:54:03 +0530

    changes for ProducerApplication

---
+++ producer-service/src/main/java/com/ravidev/controller/ProducerController.java
@@ -0,0 +1,23 @@
+package com.ravidev.controller;
+
+import com.ravidev.Model.Student;
+import com.ravidev.service.ProduceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProducerController {
+
+    @Autowired
+    ProduceService produceService;
+
+    @PostMapping(value="/produce/")
+    public ResponseEntity<String> sendData(@RequestBody Student student){
+        String msg = "Data pushed for : " + student.getName();
+        produceService.sendData(msg);
+        return ResponseEntity.ok(msg);
+    }
+}
---
+++ producer-service/src/main/java/com/ravidev/service/ProduceService.java
@@ -0,0 +1,16 @@
+package com.ravidev.service;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProduceService {
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    public void sendData(String data) {
+        rabbitTemplate.convertAndSend("direct-exchange", "queue1.key", data);
+    }
+}
