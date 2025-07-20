a179772875a69392717599ad14440738ab06cd08
rsrai <rsr7294@gmail.com>
2025-07-20 14:21:38 +0530

    changes to include other exchanges

--- producer-service/src/main/java/com/ravidev/controller/ProducerController.java
+++ producer-service/src/main/java/com/ravidev/controller/ProducerController.java
@@ -6,18 +6,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/rabbitmq")
 public class ProducerController {
 
     @Autowired
     ProduceService produceService;
 
-    @PostMapping(value="/produce/")
-    public ResponseEntity<String> sendData(@RequestBody Student student){
-        String msg = "Info for user: " + student.getName();
-        produceService.sendData(msg);
-        return ResponseEntity.ok(msg);
+    @PostMapping(value="/direct")
+    public ResponseEntity<String> sendDataViaDirectExchange(@RequestBody Student student){
+        String msg = String.format("you are using direct-exchange, %s", student.getName());
+        produceService.sendDirectExchangeData(msg);
+        return ResponseEntity.ok("Message sent using direct exchange");
+    }
+
+    @PostMapping(value="/topic")
+    public ResponseEntity<String> sendDataViaTopicExchange(@RequestBody Student student){
+        String msg = String.format("you are using topic-exchange, %s", student.getName());
+        produceService.sendTopicExchangeData(msg);
+        return ResponseEntity.ok("Message sent using topic exchange");
+    }
+
+    @PostMapping(value="/fanout")
+    public ResponseEntity<String> sendDataViaFanoutExchange(@RequestBody Student student){
+        String msg = String.format("you are using fanout-exchange, %s", student.getName());
+        produceService.sendFanoutExchangeData(msg);
+        return ResponseEntity.ok("Message sent using fanout exchange");
+    }
+
+    @PostMapping(value="/header")
+    public ResponseEntity<String> sendDataViaHeaderExchange(@RequestBody Student student){
+        String msg = String.format("you are using header-exchange, %s", student.getName());
+        produceService.sendHeadersExchangeData(msg);
+        return ResponseEntity.ok("Message sent using header exchange");
     }
 }
--- producer-service/src/main/java/com/ravidev/service/ProduceService.java
+++ producer-service/src/main/java/com/ravidev/service/ProduceService.java
@@ -1,5 +1,7 @@
 package com.ravidev.service;
 
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,7 +12,22 @@ public class ProduceService {
     @Autowired
     RabbitTemplate rabbitTemplate;
 
-    public void sendData(String data) {
+    public void sendDirectExchangeData(String data) {
         rabbitTemplate.convertAndSend("direct-exchange", "q1.key", data);
+    }
+
+    public void sendTopicExchangeData(String data) {
+        rabbitTemplate.convertAndSend("topic-exchange", "q1.#", data);
+    }
+
+    public void sendFanoutExchangeData(String data) {
+        rabbitTemplate.convertAndSend("fanout-exchange", "q1.*", data);
+    }
+    public void sendHeadersExchangeData(String data) {
+        MessageProperties messageProperties = new MessageProperties();
+        //key value here must match the one used in config to bind
+        messageProperties.setHeader("q5.key","value");
+        Message message = new Message(data.getBytes(),messageProperties);
+        rabbitTemplate.send("header-exchange", "", message);
     }
 }
